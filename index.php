63ce929d39d7a0f4c9ce640223fb4f8bf571cab8
Emil Burzo <eaglex@gmail.com>
2014-12-05 11:09:38 +0200

    refactor and modularize

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Camere Web din Maramureș</title>
+</head>
+<body>
+<?php include_once("ga.php") ?>
+
+<img src="webcam.php?id=0" width="700" height="525"/>
+<img src="webcam.php?id=1" width="700" height="525"/>
+<img src="webcam.php?id=2" width="700" height="525"/>
+<img src="webcam.php?id=3" width="700" height="525"/>
+
+</body>
+</html>
