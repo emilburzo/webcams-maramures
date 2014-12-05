8f95bc20efe91953dc532e0fa8603347c7d73a67
Emil Burzo <eaglex@gmail.com>
2014-12-05 11:21:38 +0200

    add sanity checks and comments

--- webcam.php
+++ webcam.php
@@ -1,7 +1,6 @@
 <?php
 
-header("Content-Type: image/jpeg");
-
+// webcam list
 $array = [
     "0" => "http://jurnalul.ro/webcam/suior-1-427.html",
     "1" => "http://jurnalul.ro/webcam/suior-2-428.html",
@@ -9,9 +8,18 @@ $array = [
     "3" => "http://jurnalul.ro/webcam/cavnic-roata-3-303.html",
 ];
 
-$id = $_GET["id"];
+// webcam id parameter
+$id = htmlspecialchars($_GET["id"]);
+
+if (!array_key_exists($id, $array)) {
+    header("Content-Type: text/plain");
+    echo "Nice try.";
+    return;
+}
+
 $url = $array[$id];
 
+// fetch webcam URL content and save cookies file (webcam URL is tied to the session)
 $ch = curl_init($url);
 
 $cookie_file = "/tmp/cookies-" . rand();
@@ -25,6 +33,7 @@ $content = curl_exec($ch);
 
 curl_close($ch);
 
+// parse webcam URL
 $findme = "hide_cam_url.php";
 $pos1 = strpos($content, $findme);
 
@@ -35,6 +44,7 @@ $relative_url = substr($content, $pos1, ($pos2 - $pos1 - 6));
 
 //echo $relative_url;
 
+// fetch image
 $ch = curl_init("http://jurnalul.ro/" . $relative_url);
 
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -46,6 +56,7 @@ $image = curl_exec($ch);
 
 curl_close($ch);
 
+header("Content-Type: image/jpeg");
 echo $image;
 
 unlink($cookie_file);
