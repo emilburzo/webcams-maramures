cc443fdae3f9fa01a9f2baf862be7a053ea3832c
Emil Burzo <eaglex@gmail.com>
2014-12-05 15:42:00 +0200

    change title

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Camere Web din Maramureș</title>
+    <title>Camere Web - Maramureș</title>
 </head>
 <body>
 <?php include_once("ga.php") ?>
