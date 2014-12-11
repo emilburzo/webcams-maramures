3a2c758f52e4f37ab5fa1d6bcdd3d680f5e86cc3
Emil Burzo <eaglex@gmail.com>
2014-12-11 19:57:37 +0200

    fix php function call

--- index.php
+++ index.php
@@ -8,9 +8,9 @@
 <body>
 <?php include_once("ga.php") ?>
 
-<img src="http://suior.dyndns.org:3334/cgi-bin/faststream.jpg?stream=full&fps=1&rand=<? echo $rand(); ?>" width="700"
+<img src="http://suior.dyndns.org:3334/cgi-bin/faststream.jpg?stream=full&fps=1&rand=<?php echo rand(); ?>" width="700"
      height="525"/>
-<img src="http://suior.dyndns.org:3335/cgi-bin/faststream.jpg?stream=full&fps=1&rand=<? echo $rand(); ?>" width="700"
+<img src="http://suior.dyndns.org:3335/cgi-bin/faststream.jpg?stream=full&fps=1&rand=<?php echo rand(); ?>" width="700"
      height="525"/>
 
 </body>
