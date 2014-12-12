f229aaca293c0668e3a554ffc6b60e0718a446b6
Emil Burzo <eaglex@gmail.com>
2014-12-12 16:48:46 +0200

    add RCS&RDS flash webcam

--- index.php
+++ index.php
@@ -8,10 +8,9 @@
 <body>
 <?php include_once("ga.php") ?>
 
-<img src="http://suior.dyndns.org:3334/cgi-bin/faststream.jpg?stream=full&fps=1&rand=<?php echo rand(); ?>" width="700"
-     height="525"/>
-<img src="http://suior.dyndns.org:3335/cgi-bin/faststream.jpg?stream=full&fps=1&rand=<?php echo rand(); ?>" width="700"
-     height="525"/>
+<img src="http://suior.dyndns.org:3334/cgi-bin/faststream.jpg?stream=full&fps=1&rand=<?php echo rand(); ?>" width="700" height="525"/>
+<img src="http://suior.dyndns.org:3335/cgi-bin/faststream.jpg?stream=full&fps=1&rand=<?php echo rand(); ?>" width="700" height="525"/>
+<iframe src="http://www.rcs-rds.ro/cams_utilities/jwplayer/player-licensed53.swf?streamer=rtmpe://82.76.249.73/digilivedge&file=baia_mare_desktop" width="700" height="525">
 
 </body>
 </html>
