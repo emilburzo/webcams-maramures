0cce2704822f8cf1c5e4c651f9b129cb1a482b9e
Emil Burzo <eaglex@gmail.com>
2014-12-12 20:04:50 +0200

    jurnalul.ro is back up, revert changes

--- index.php
+++ index.php
@@ -4,13 +4,15 @@
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Camere Web - Maramureș</title>
+    <script type="text/javascript" src="js/script.js"></script>
 </head>
-<body>
+<body onload="start();">
 <?php include_once("ga.php") ?>
 
-<img src="http://suior.dyndns.org:3334/cgi-bin/faststream.jpg?stream=full&fps=1&rand=<?php echo rand(); ?>" width="700" height="525"/>
-<img src="http://suior.dyndns.org:3335/cgi-bin/faststream.jpg?stream=full&fps=1&rand=<?php echo rand(); ?>" width="700" height="525"/>
+<img src="webcam.php?id=0" width="700" height="525"/>
+<img src="webcam.php?id=1" width="700" height="525"/>
+<img src="webcam.php?id=2" width="700" height="525"/>
+<img src="webcam.php?id=3" width="700" height="525"/>
 <iframe src="http://www.rcs-rds.ro/cams_utilities/jwplayer/player-licensed53.swf?streamer=rtmpe://82.76.249.73/digilivedge&file=baia_mare_desktop" width="700" height="525">
-
 </body>
 </html>
