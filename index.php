f31ff00e7b0c77521f939648abfaa950ff5c5e38
Emil Burzo <contact@emilburzo.com>
2016-12-12 19:36:28 +0200

    add a new Cavnic stream

--- index.php
+++ index.php
@@ -7,26 +7,54 @@
 
     <title>Camere Web din Maramureș</title>
     <meta name=”description” content='Webcam-uri în direct din zona Maramureș, în special de la munte'>
-    <meta name=”keywords” content='webcam, maramureș, munte, baia mare, baia sprie, romania, munte, mountain, cavnic, suior, ignis, varful ignis'>
+    <meta name=”keywords” content='webcam, maramureș, munte, baia mare, baia sprie, romania, munte, cavnic, suior, ignis, varful ignis'>
 
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
     <link rel="icon" href="/favicon.ico" type="image/x-icon">
 
     <script type="text/javascript" src="js/script.js"></script>
     <link href="css/style.css" rel="stylesheet">
+
+    <script src="js/superski/video.js"></script>
+    <script src="js/superski/videojs-contrib-hls.min.js"></script>
 </head>
 <body onload="start();">
 
+<header>
+    <p>Webcam-uri în direct din zona Maramureș (Baia Mare, Baia Sprie, Cavnic), în special de la munte (Șuior, Cavnic, vârful Igniș)</p>
+</header>
+
 <div class="cam">
-    <img class="skip" title="Suior" src="http://suior.dyndns.org:3334/cgi-bin/faststream.jpg?stream=full&fps=5&rand=<?php echo time(); ?>" width="504" height="376"/>
+    <img class="skip" title="Suior" src="http://suior.dyndns.org:3334/cgi-bin/faststream.jpg?stream=full&fps=3&rand=<?php echo time(); ?>" width="504" height="376"/>
 </div>
 
 <div class="cam">
-    <img class="skip" title="Suior" src="http://suior.dyndns.org:3335/control/faststream.jpg?stream=full&fps=5&rand=<?php echo time(); ?>" width="504" height="376"/>
+    <img class="skip" title="Suior" src="http://suior.dyndns.org:3335/control/faststream.jpg?stream=full&fps=3&rand=<?php echo time(); ?>" width="504" height="376"/>
 </div>
 
 <div class="cam">
-    <img class="skip" title="Suior" src="http://suior.dyndns.org:3333/cgi-bin/faststream.jpg?stream=full&fps=5&rand=<?php echo time(); ?>" width="504" height="376"/>
+    <img class="skip" title="Suior" src="http://suior.dyndns.org:3333/cgi-bin/faststream.jpg?stream=full&fps=3&rand=<?php echo time(); ?>" width="504" height="376"/>
+</div>
+
+<div class="cam">
+    <video id=skivid controls title="Cavnic" style="width: 504px !important; height: 376px !important;">
+        <source src="http://rtmp.streamaxia.com/ski/ski.stream/playlist.m3u8" type="application/x-mpegURL">
+    </video>
+
+    <script>
+        var player = videojs('skivid', {
+            controlBar: false,
+            bigPlayButton: false,
+            loadingSpinner: false,
+            textTrackDisplay: false,
+            errorDisplay: false,
+            textTrackSettings: false,
+            posterImage: false,
+            nativeControlsForTouch: true
+        });
+        player.userActive(true);
+        player.play();
+    </script>
 </div>
 
 <div class="cam">
