68dfa46613ea07562a4be78707479553de838fcf
Emil Burzo <contact@emilburzo.com>
2021-07-03 10:48:48 +0300

    create jwplayer cam templating function

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+include 'functions.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -21,9 +24,8 @@
 <body>
 
 <header>
-    <p>Webcam-uri în direct din zona Maramureș (Baia Mare, Tăuții-Măgherăuș, Baia Sprie, Cavnic, Baraj Firiza, Borșa), în
-        special de la munte
-        (Șuior, Lacul Mogoșa / Bodi, Cavnic, <strike>vârful Igniș</strike>, Telescaun Borșa)</p>
+    <p>Webcam-uri în direct din zona Maramureș (Baia Mare, Tăuții-Măgherăuș, Baia Sprie, Cavnic, Baraj Firiza, Borșa),
+        în special de la munte (Șuior, Lacul Mogoșa / Bodi, Cavnic, <strike>vârful Igniș</strike>, Telescaun Borșa)</p>
 </header>
 
 <div id="webcam_suior_1_wrapper" class="cam">
@@ -50,164 +52,22 @@
          onerror="document.getElementById('webcam_suior_4_wrapper').style.display = 'none';"/>
 </div>
 
-<div id="webcam_bm_wrapper" class="cam">
-    <div id="webcam_bm" title="Baia Mare"></div>
+<?php jwplayer_cam("webcam_bm", "Baia Mare", "https://digilive.rcs-rds.ro/digilivedge/baia_mare_desktop.stream/index.m3u8"); ?>
 
-    <script>
-        $(function () {
-            var id = 'webcam_bm';
-            var player = jwplayer(id);
-            player.setup({
-                file: 'http://82.76.249.73/digilivedge/baia_mare_desktop.stream/index.m3u8',
-                height: 376,
-                width: 504,
-                autostart: true,
-                mute: true,
-                title: "Baia Mare (by RCS&RDS)"
-            });
-            player.on('error', function () {
-                document.getElementById(id + '_wrapper').style.display = 'none';
-            });
-        });
-    </script>
-</div>
+<?php jwplayer_cam("webcam_aeroclub_bm", "Tăuții-Măgherăuș - Aeroclubul Baia Mare", "https://live2.freecam.ro:5443/LiveApp/streams/222562831095066913214038.m3u8"); ?>
 
-<div id="webcam_tauti_wrapper" class="cam">
-    <div id="webcam_tauti" title="Tăuții-Măgherăuș"></div>
-
-    <script>
-        $(function () {
-            var id = 'webcam_tauti';
-            var player = jwplayer(id);
-            player.setup({
-                file: 'https://live.freecam.ro:5443/LiveApp/streams/949078194090490060693346.m3u8',
-                height: 376,
-                width: 504,
-                autostart: true,
-                mute: true,
-                title: "Tăuții-Măgherăuș"
-            });
-            player.on('error', function () {
-                document.getElementById(id + '_wrapper').style.display = 'none';
-            });
-        });
-    </script>
-</div>
-
-<div id="webcam_simared_wrapper" class="cam">
-    <div id="webcam_simared" title="Baraj Firiza - Simared"></div>
-
-    <script>
-        $(function () {
-            var id = 'webcam_simared';
-            var player = jwplayer(id);
-            player.setup({
-                file: 'https://live.simared.ro/camera1/stream.m3u8',
-                height: 376,
-                width: 504,
-                autostart: true,
-                mute: true,
-                title: "Baraj Firiza - Simared"
-            });
-            player.on('error', function () {
-                document.getElementById(id + '_wrapper').style.display = 'none';
-            });
-        });
-    </script>
-</div>
-
-<div id="webcam_baiasprie_wrapper" class="cam">
-    <div id="webcam_baiasprie" title="Baia Sprie"></div>
-
-    <script>
-        $(function () {
-            var id = 'webcam_baiasprie';
-            var player = jwplayer(id);
-            player.setup({
-                file: 'http://link7.ro:8080/hls/live.stream.m3u8',
-                height: 376,
-                width: 504,
-                autostart: true,
-                mute: true,
-                title: "Baia Sprie"
-            });
-            player.on('error', function () {
-                document.getElementById(id + '_wrapper').style.display = 'none';
-            });
-        });
-    </script>
-</div>
+<?php jwplayer_cam("webcam_simared", "Baraj Firiza - Simared", "https://live.simared.ro/camera1/stream.m3u8"); ?>
 
 <div title="Lacul Mogoșa" class="cam">
     <iframe width=504 height=376 src="https://play.webcamromania.ro/b3p4l5g5v534o2v223" allowfullscreen
             name="Lacul Mogoșa" scrolling="no" seamless="seamless" frameborder="0"></iframe>
 </div>
 
-<div id="webcam_cavnic_wrapper" class="cam">
-    <div id="webcam_cavnic" title="Cavnic - Roata 1"></div>
+<?php jwplayer_cam("webcam_cavnic", "Cavnic - Roata 1", "https://live.freecam.ro:5443/LiveApp/streams/238240691967830141202578.m3u8"); ?>
 
-    <script>
-        $(function () {
-            var id = 'webcam_cavnic';
-            var player = jwplayer(id);
-            player.setup({
-                file: 'https://live.freecam.ro:5443/LiveApp/streams/238240691967830141202578.m3u8',
-                height: 376,
-                width: 504,
-                autostart: true,
-                mute: true,
-                title: "SuperSki Cavnic - Roata 1"
-            });
-            player.on('error', function () {
-                document.getElementById(id + '_wrapper').style.display = 'none';
-            });
-        });
-    </script>
-</div>
+<?php jwplayer_cam("webcam_cavnic2", "Cavnic - Roata 2", "https://live.freecam.ro:5443/LiveApp/streams/298364518216581967384953.m3u8"); ?>
 
-<div id="webcam_cavnic2_wrapper" class="cam">
-    <div id="webcam_cavnic2" title="Cavnic - Roata 2"></div>
-
-    <script>
-        $(function () {
-            var id = 'webcam_cavnic2';
-            var player = jwplayer(id);
-            player.setup({
-                file: 'https://live.freecam.ro:5443/LiveApp/streams/298364518216581967384953.m3u8',
-                height: 376,
-                width: 504,
-                autostart: true,
-                mute: true,
-                title: "SuperSki Cavnic - Roata 2"
-            });
-            player.on('error', function () {
-                document.getElementById(id + '_wrapper').style.display = 'none';
-            });
-        });
-    </script>
-</div>
-
-<div id="webcam_cavnic3_wrapper" class="cam">
-    <div id="webcam_cavnic3" title="Cavnic - Roata 3"></div>
-
-    <script>
-        $(function () {
-            var id = 'webcam_cavnic3';
-            var player = jwplayer(id);
-            player.setup({
-                file: 'https://live.freecam.ro:5443/LiveApp/streams/937528909720485946133487.m3u8',
-                height: 376,
-                width: 504,
-                autostart: true,
-                mute: true,
-                title: "SuperSki Cavnic - Roata 3"
-            });
-            player.on('error', function () {
-                document.getElementById(id + '_wrapper').style.display = 'none';
-            });
-        });
-    </script>
-</div>
+<?php jwplayer_cam("webcam_cavnic3", "Cavnic - Roata 3", "https://live.freecam.ro:5443/LiveApp/streams/937528909720485946133487.m3u8"); ?>
 
 <div title="Cavnic - Icoana" class="cam">
     <iframe width=504 height=376 src="https://play.webcamromania.ro/b3p4l5g5v534o21353" allowfullscreen
@@ -218,12 +78,6 @@
     <iframe width=504 height=376 src="https://play.webcamromania.ro/b3p4l5g5v534o2w223" allowfullscreen
             name="Borșa - Telescaun" scrolling="no" seamless="seamless" frameborder="0"></iframe>
 </div>
-
-<!--<div id="webcam_borsa_prislop" class="cam">-->
-<!--    <img width=504 height=376 title="Borșa - Pasul Prislop"-->
-<!--         src="https://image.webcamromania.ro/camera/030/snap/big.jpg?rand=1609918812"-->
-<!--         onerror="document.getElementById('webcam_borsa_prislop').style.display = 'none';"/>-->
-<!--</div>-->
 
 <script type="text/javascript" src="js/jwplayer.js"></script>
 <script>jwplayer.key = "jScWsLuA6KaZwo3HVTDeYjOBtJsY3/SdyB6BkQ==";</script>
